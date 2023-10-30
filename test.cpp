7e6a808d43b4f2c6b2cdb7920f09f0c60dd6ff34
koskim1 <koskim1@naver.com>
2023-10-30 16:54:51 +0900

    stack

--- test.cpp
+++ test.cpp
@@ -1,18 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-multiset<int> s;
+stack<string> stk;
 
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    for (int i = 5; i >= 1; i--) {
-        s.insert(i);
-        s.insert(i);
+    stk.push("엄");
+    stk.push("준");
+    stk.push("식");
+    stk.push("화");
+    stk.push("이");
+    stk.push("팅");
+    
+    while (stk.size()) {
+        cout << stk.top() << "\n";
+        stk.pop();
     }
 
-    for (int it : s) cout << it << " ";
-    cout << "\n";
-
 }
+/*
+팅
+이
+화
+식
+준
+엄
+*/

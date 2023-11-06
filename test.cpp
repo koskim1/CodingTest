3833161df8c66eba493dc697dea5370348f06e1d
koskim1 <koskim1@naver.com>
2023-11-06 15:50:00 +0900

    Call by Reference

--- test.cpp
+++ test.cpp
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int add(int a, int b) {
+int add(int& a, int b) {
     a += 10;
     cout << a << "\n";
     return a + b;
 }
 
 vector<int> v(3, 0);
-void go(vector<int> v) {
-    v[1] = 100;
+void go(vector<int>& v) {
+    v[2] = 100;
 }
 
 int main(void) {
@@ -19,10 +19,15 @@ int main(void) {
     int a = 1;
     int b = 2;
     int sum = add(a, b);
-    cout << a << '\n';
+    cout << a << "\n";
 
-
-    // vector를 값에 의한 호출을 하면 수정해도 아무런 반영 되지 않음. 위와 마찬가지.
     go(v);
     for (int i : v) cout << i << "\n";
 }
+/*
+11
+11
+0
+0
+100
+*/

a52efede2e189b4dc4715d164d4d68e8bf205749
koskim1 <koskim1@naver.com>
2023-11-07 00:20:13 +0900

    참조에 의한 호출로 넘겨야 할 때 ( 백준 2776번문제 포함 )

--- test.cpp
+++ test.cpp
@@ -1,28 +1,37 @@
 #include <bits/stdc++.h>
+#include <unordered_map>
 using namespace std;
 
-int add(int& a, int b) {
-    a += 10;
-    cout << a << "\n";
-    return a + b;
-}
+typedef long long ll;
+int t, n, m, temp;
 
-vector<int> v(3, 0);
-void go(vector<int>& v) {
-    v[2] = 100;
+unordered_map<int, int> mp;
+
+int check(int temp, vector<int>& v) {
+    int l = 0, r = v.size() - 1;
+    int mid;
+    while (l <= r) {
+        mid = (l + r) / 2;
+        if (v[mid] > temp)
+            r = mid - 1;
+        else if (v[mid] == temp) return 1;
+        else l = mid + 1;
+    }
+    return 0;
 }
 
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int a = 1;
-    int b = 2;
-    int sum = add(a, b);
-    cout << a << "\n";
-
-    go(v);
-    for (int i : v) cout << i << "\n";
+    scanf_s("%d", &t);
+    while (t--) {
+        vector<int> v;
+        scanf_s("%d", &n); for (int i = 0; i < n; i++)scanf_s("%d", &temp), v.push_back(temp);
+        sort(v.begin(), v.end());
+        scanf_s("%d", &m); for (int i = 0; i < m; i++)scanf_s("%d", &temp), cout << check(temp, v) << "\n";
+    }
+    return 0;
 }
 /*
 11

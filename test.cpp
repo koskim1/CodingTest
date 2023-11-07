1ae2718ea6b5374a721660f6906f895e16783a98
koskim1 <koskim1@naver.com>
2023-11-08 00:02:32 +0900

    Array의 요소 수정하기

--- test.cpp
+++ test.cpp
@@ -1,42 +1,34 @@
 #include <bits/stdc++.h>
-#include <unordered_map>
 using namespace std;
 
-typedef long long ll;
-int t, n, m, temp;
+int a[3] = { 1,2,3 };
+void go(int a[]) {
+    a[2] = 100;
+}
 
-unordered_map<int, int> mp;
+void go2(int a[3]) {
+    a[2] = 1000;
+}
 
-int check(int temp, vector<int>& v) {
-    int l = 0, r = v.size() - 1;
-    int mid;
-    while (l <= r) {
-        mid = (l + r) / 2;
-        if (v[mid] > temp)
-            r = mid - 1;
-        else if (v[mid] == temp) return 1;
-        else l = mid + 1;
-    }
-    return 0;
+void go3(int* a) {
+    a[2] = 10000;
 }
 
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    scanf_s("%d", &t);
-    while (t--) {
-        vector<int> v;
-        scanf_s("%d", &n); for (int i = 0; i < n; i++)scanf_s("%d", &temp), v.push_back(temp);
-        sort(v.begin(), v.end());
-        scanf_s("%d", &m); for (int i = 0; i < m; i++)scanf_s("%d", &temp), cout << check(temp, v) << "\n";
+    go(a); cout << a[2] << '\n';
+    go2(a); cout << a[2] << '\n';
+    go3(a); cout << a[2] << '\n';
+    for (auto v : a) {
+        cout << v << " ";
     }
-    return 0;
+    cout << "\n";
 }
 /*
-11
-11
-0
-0
 100
+1000
+10000
+1 2 10000
 */

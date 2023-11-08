6dfa4311450903f7985aa0c05b94793466df4740
koskim1 <koskim1@naver.com>
2023-11-08 20:35:27 +0900

    2차원 배열 수정하기 - vector

--- test.cpp
+++ test.cpp
@@ -1,34 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int a[3] = { 1,2,3 };
-void go(int a[]) {
-    a[2] = 100;
+vector<vector<int>> v;
+vector<vector<int>> v2(10, vector<int>(10, 0));
+vector<int> v3[10];
+
+void go(vector<vector<int>>& v) {
+    v[0][0] = 100;
 }
 
-void go2(int a[3]) {
-    a[2] = 1000;
+void go2(vector<vector<int>>& v) {
+    v[0][0] = 1000;
 }
 
-void go3(int* a) {
-    a[2] = 10000;
+void go3(vector<int> v[10]) {
+    v[0][0] = 10000;
 }
 
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    go(a); cout << a[2] << '\n';
-    go2(a); cout << a[2] << '\n';
-    go3(a); cout << a[2] << '\n';
-    for (auto v : a) {
-        cout << v << " ";
-    }
-    cout << "\n";
+    vector<int> temp;
+    temp.push_back(0);
+    v.push_back(temp);
+
+    v3[0].push_back(0);
+
+    go(v); go2(v2); go3(v3);
+    cout << v[0][0] << "\n";
+    cout << v2[0][0] << "\n";
+    cout << v3[0][0] << "\n";
+
 }
 /*
 100
 1000
 10000
-1 2 10000
 */

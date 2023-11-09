b76c106596f16fbea9fe964b1350910538e99c7c
koskim1 <koskim1@naver.com>
2023-11-09 23:19:07 +0900

    순열과 조합 - next_permutation과 prev_permutation

--- test.cpp
+++ test.cpp
@@ -1,35 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int fibo(int n) {
-    cout << "fibo : " << n << "\n";
-    if (n == 0 || n == 1) return n;
-    return fibo(n - 1) + fibo(n - 2);
+void printV(vector<int>& v) {
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << " ";
+    }
+    cout << "\n";
 }
-
-int n = 5;
 
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    cout << fibo(n) << "\n";
+    int a[3] = { 1,2,3 };
+    vector<int> v;
+
+    for (int i = 0; i < 3; i++)v.push_back(a[i]);
+    //1,2,3 부터 오름차순으로 순열을 뽑습니다.
+    do {
+        printV(v);
+    } while (next_permutation(v.begin(), v.end()));
+    cout << "-----------------" << "\n";
+    v.clear();
+    for (int i = 2; i >= 0; i--)v.push_back(a[i]);
+    // 3,2,1 부터 내림차순으로 순열을 뽑습니다.
+    do {
+        printV(v);
+    } while (prev_permutation(v.begin(), v.end()));
+    return 0;
 }
 /*
-fibo : 5
-fibo : 4
-fibo : 3
-fibo : 2
-fibo : 1
-fibo : 0
-fibo : 1
-fibo : 2
-fibo : 1
-fibo : 0
-fibo : 3
-fibo : 2
-fibo : 1
-fibo : 0
-fibo : 1
-5
+1 2 3
+1 3 2
+2 1 3
+2 3 1
+3 1 2
+3 2 1
+-----------------
+3 2 1
+3 1 2
+2 3 1
+2 1 3
+1 3 2
+1 2 3
 */

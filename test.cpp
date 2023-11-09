e2d9e30e1fb1c47f4681ab9e667742cd7692f62b
koskim1 <koskim1@naver.com>
2023-11-09 23:19:33 +0900

    next_permutation() 사용시 주의할 점

--- test.cpp
+++ test.cpp
@@ -1,46 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void printV(vector<int>& v) {
-    for (int i = 0; i < v.size(); i++) {
-        cout << v[i] << " ";
-    }
-    cout << "\n";
-}
-
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int a[3] = { 1,2,3 };
-    vector<int> v;
+    cout << "정렬되어있지 않은 배열기반\n";
+    int a[] = { 1,3,2 };
+    do {
+        for (int i : a) cout << i << " ";
+        cout << "\n";
+    } while (next_permutation(a, a + 3));
+    cout << "정렬된 배열기반\n";
+    int b[] = { 1,2,3 };
+    do {
+        for (int i : b)cout << i << " ";
+        cout << "\n";
+    } while (next_permutation(b, b + 3));
 
-    for (int i = 0; i < 3; i++)v.push_back(a[i]);
-    //1,2,3 부터 오름차순으로 순열을 뽑습니다.
-    do {
-        printV(v);
-    } while (next_permutation(v.begin(), v.end()));
-    cout << "-----------------" << "\n";
-    v.clear();
-    for (int i = 2; i >= 0; i--)v.push_back(a[i]);
-    // 3,2,1 부터 내림차순으로 순열을 뽑습니다.
-    do {
-        printV(v);
-    } while (prev_permutation(v.begin(), v.end()));
-    return 0;
 }
 /*
+정렬되어있지 않은 배열기반
+1 3 2
+2 1 3
+2 3 1
+3 1 2
+3 2 1
+정렬된 배열기반
 1 2 3
 1 3 2
 2 1 3
 2 3 1
 3 1 2
 3 2 1
------------------
-3 2 1
-3 1 2
-2 3 1
-2 1 3
-1 3 2
-1 2 3
+
 */

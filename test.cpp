5bea6f7e6be047bb74c4e6a441ee29857fb3cf39
koskim1 <koskim1@naver.com>
2023-11-13 21:21:04 +0900

    재귀를 이용한 순열 ( ★ 원복하는 부분 확실히 이해 하기 ★ )

--- test.cpp
+++ test.cpp
@@ -1,16 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int a[3] = { 1,2,3 };
+int n = 3, r = 3; // r을 바꿔가면서 연습해보세요. :)
+
+void print() {
+    for (int i = 0; i < r; i++) {
+        cout << a[i] << " ";
+    }
+    cout << "\n";
+}
+
+void makePermutation(int n, int r, int depth) {
+    if (r == depth) {
+        print();
+        return;
+    }
+    for (int i = depth; i < n; i++) {
+        cout << i << " : " << depth << "를 바꾼다!\n";
+        swap(a[i], a[depth]);
+        makePermutation(n, r, depth + 1);
+        cout << i << " : " << depth << "를 다시 바꾼다!\n";
+        swap(a[i], a[depth]);
+    }
+    return;
+}
+
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    vector<int> a = { 2,1,3 };
-    sort(a.begin(), a.end());
-
-    do {
-        for (int i : a) cout << i << " ";
-        cout << "\n";
-    } while (next_permutation(a.begin(), a.end()));
+    makePermutation(n, r, 0);
 
 }

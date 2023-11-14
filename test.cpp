4291797cf55d273757452d8531723885801bdb9f
koskim1 <koskim1@naver.com>
2023-11-14 23:26:11 +0900

    조합 중첩 for 문

--- test.cpp
+++ test.cpp
@@ -3,30 +3,17 @@ using namespace std;
 
 int n = 5, k = 3, a[5] = { 1,2,3,4,5 };
 
-void print(vector<int> b) {
-    for (int i : b) cout << i << " ";
-    cout << '\n';
-}
-
-void combi(int start, vector<int> b) {
-    if (b.size() == k) {
-        print(b);
-        return;
-    }
-    for (int i = start + 1; i < n; i++) {
-        b.push_back(i);
-        combi(i, b);
-        b.pop_back();
-    }
-    return;
-}
-
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    vector<int> b;
-    combi(-1, b);
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            for (int k = j + 1; k < n; k++) {
+                cout << i << " " << j << " " << k << '\n';
+            }
+        }
+    }
 
 }
 /*

ce1590861234ffc3055e3a69535c29e593e86c56
koskim1 <koskim1@naver.com>
2023-11-15 11:16:57 +0900

    최대공약수(gcd)와 최소공배수(lcm)

--- test.cpp
+++ test.cpp
@@ -1,30 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n = 5, k = 3, a[5] = { 1,2,3,4,5 };
+int gcd(int a, int b) {
+    if (a == 0) return b;
+    return gcd(b % a, a);
+}
+
+int lcm(int a, int b) {
+    return (a * b) / gcd(a, b);
+}
 
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    for (int i = 0; i < n; i++) {
-        for (int j = i + 1; j < n; j++) {
-            for (int k = j + 1; k < n; k++) {
-                cout << i << " " << j << " " << k << '\n';
-            }
-        }
-    }
+    int a = 10, b = 12;
+    cout << lcm(a, b) << "\n";
 
+    return 0;
 }
 /*
-0 1 2
-0 1 3
-0 1 4
-0 2 3
-0 2 4
-0 3 4
-1 2 3
-1 2 4
-1 3 4
-2 3 4
+60
 */

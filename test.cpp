8399020a135561e41a2edecf58393011e3b823c5
koskim1 <koskim1@naver.com>
2023-11-16 14:30:46 +0900

    등비수열의 합

--- test.cpp
+++ test.cpp
@@ -1,14 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int n = 5;
-    int first = 3, last = 23;
-    cout << n * (first + last) / 2 << "\n";
+    // 초항 a, 배수 r, 갯수 n
+    int a = 1, r = 2, n = 4;
+
+    vector<int> v;
+    cout << a * ((int)pow(r, n) - 1) / (r - 1) << "\n";
+    /*
+    pow(r, n)은 r의 n승을 뜻함
+    기본적으로 암묵적 형변환에 의해 float형식으로 들어가지만
+    그래서 int형식으로 형변환을 시켜줘야함.
+    */
+    for (int i = 0; i < n; i++) {
+        v.push_back(a);
+        a *= r;
+    }
+    for (int i : v) cout << i << ' ';
 }
 /*
-65
+15
+1 2 4 8
 */

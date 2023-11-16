b791110819ec5839d9b8c94bf274f72677b23434
koskim1 <koskim1@naver.com>
2023-11-16 14:31:16 +0900

    승수 ( ~의 n승 )

--- test.cpp
+++ test.cpp
@@ -6,23 +6,15 @@ int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    // 초항 a, 배수 r, 갯수 n
-    int a = 1, r = 2, n = 4;
+    int n = 4;
+    int pow_2 = (int)pow(2, n);
+    cout << pow_2 << "\n";
 
-    vector<int> v;
-    cout << a * ((int)pow(r, n) - 1) / (r - 1) << "\n";
-    /*
-    pow(r, n)은 r의 n승을 뜻함
-    기본적으로 암묵적 형변환에 의해 float형식으로 들어가지만
-    그래서 int형식으로 형변환을 시켜줘야함.
-    */
-    for (int i = 0; i < n; i++) {
-        v.push_back(a);
-        a *= r;
-    }
-    for (int i : v) cout << i << ' ';
+    int pow_3 = pow(2.0, 4.0);
+    cout << pow_3 << "\n";
+
 }
 /*
-15
-1 2 4 8
+16
+16
 */

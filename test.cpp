5d73de76e5749302c559cd9ba42224710b38d329
koskim1 <koskim1@naver.com>
2023-11-18 23:12:04 +0900

    rotate() - 반시계방향 일부분만

--- test.cpp
+++ test.cpp
@@ -7,10 +7,10 @@ int main(void) {
     cin.tie(0);
 
     vector<int> v = { 1,2,3,4,5,6 };
-    rotate(v.begin(), v.begin() + 1, v.end());
+    rotate(v.begin() + 1, v.begin() + 2, v.begin() + 5);
     for (int i : v) cout << i << ' ';
 
 }
 /*
-2 3 4 5 6 1
+1 3 4 5 2 6
 */

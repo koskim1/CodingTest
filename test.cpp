44420bceb4a526b256ded5c619b0746aee7e8dc0
koskim1 <koskim1@naver.com>
2023-11-18 23:13:17 +0900

    2차원 배열 회전 - 행과 열이 같은 값일 때

--- test.cpp
+++ test.cpp
@@ -1,45 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int n = 3;
-const int m = 4;
-
-void rotate_left_90(vector<vector<int>>& key) {
-    int n = key.size();
-    int m = key[0].size();
-    vector<vector<int>> temp(m, vector<int>(n, 0));
-    /*
-    0 0 0
-    0 0 0
-    0 0 0
-    0 0 0
-    */
-
-    for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
-            temp[i][j] = key[j][m - i - 1];
-        }
-    }
-    key.resize(m);
-    key[0].resize(n);
-
-    key = temp;
-    return;
-}
+const int n = 4;
 
 void rotate_right_90(vector<vector<int>>& key) {
-    int n = key.size();
-    int m = key[0].size();
-    vector<vector<int>> temp(m, vector<int>(n, 0));
-
-    for (int i = 0; i < m; i++) {
+    vector<vector<int>> temp(n, vector<int>(n, 0));
+    for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             temp[i][j] = key[n - j - 1][i];
+            // µð¹ö±×
+            cout << temp[i][j] << '\n';
         }
     }
-    key.resize(m);
-    key[0].resize(n);
-
     key = temp;
     return;
 }
@@ -51,20 +23,21 @@ int main(void) {
     vector<vector<int>> a = {
         {1,2,3,4},
         {5,6,7,8},
-        {9,10,11,12}
+        {9,10,11,12},
+        {13,14,15,16},
     };
     rotate_right_90(a);
-    for (int i = 0; i < m; i++) {
+    for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cout << a[i][j] << " ";
         }
-        cout << "\n";
+        cout << '\n';
     }
     return 0;
 }
 /*
-9 5 1
-10 6 2
-11 7 3
-12 8 4
+13 9 5 1
+14 10 6 2
+15 11 7 3
+16 12 8 4
 */

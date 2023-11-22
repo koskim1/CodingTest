815630c098cea0d3fe5466f2f24e3b4704b1a235
koskim1 <koskim1@naver.com>
2023-11-22 17:12:32 +0900

    입출력 싱크

--- test.cpp
+++ test.cpp
@@ -6,44 +6,22 @@ int main(void) {
     cin.tie(0);
 
 /*
-* 
-bits/stdc++.h는 모든 라이브러리를 로드하기 때문에 다른 라이브러리에서 전역변수로 쓰고
-있는 변수명을 쓰지 못합니다.
-따라서 해당 변수명 같은 경우 그럴 경우에는 define을 써주어야 합니다. define을 걸어서
-다른 임의의 문자열로 바꾸어 버린다음 다음 코드처럼 구축하면 됩니다.
+# 입출력 싱크
 
-#define y1 xoxoxoxo
+입출력 싱크를 풀어서 cin, cout의 속도를 빠르게 설정하는 방법이 있습니다. 시간초과가 났을 때 다음코드처럼 구축한다면 시간초과가 통과하는 경우도 있습니다.
 
-대표적으로 쓰지 못하는 변수명으로는 다음과 같습니다.
-● y1
-● time
-● prev
-● next
-예시는 다음과 같습니다.
 
-#include <bits/stdc++.h>
-using namespace std;
-#define prev aaa
-#define next aaaa
-int prev[4];
-int main() {
-    cout << prev[0] << '\n';
-    return 0;
-}
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+cout.tie(NULL);
 
-또한 보통 함수로 사용되는 이름이나 매개변수로 들어가는 이터레이터의 이름 등은
-define을 걸어도 쓰지 못합니다. 즉, 해당 부분은 변수명으로 선언하면 안됩니다.
-예를 들어 다음코드에서 주석처리를 해제해서 sort를 하게 되면 에러가 발생됩니다. sort의
-end라는 이터레이터를 사용하기 때문이죠.
 
-#include <bits/stdc++.h>
-using namespace std;
-#define end aaa
-vector<int> v;
-int main() {
-    for(int i : {1, 2, 3, 4, 5}) v.push_back(i);
-    //sort(v.begin(), v.end());
-}
+C++에서 대표적인 입출력 함수로는 cin, sout, scanf, printf가 있고 이는 시스템콜관련 함수이므로 같이 쓰일 때는 입출력싱크를 맞춰주어야 합니다.
 
+보통은 cin, cout과 scanf, printf중 scanf, printf가 빠릅니다. 왜냐하면 cin, cout은 c라이브러리인 stdio의 버퍼와 동기화하느라 시간을 소비하기 때문이죠.
+
+이 때 앞의 코드처럼 싱크를 해제하여 버퍼 동기화를 하지 않게 하여 cin, cout의 구동속도를 빠르게 해줄 수 있습니다. 하지만 이렇게 했을 때 반드시!! cin, cout를 쓸 때 scanf, printf를 쓰지 말아야 합니다.
+
+앞의 코드를 쓴다면 반드시 cin, cout만 써야 합니다.
 */
 }

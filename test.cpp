e144a4ff2b81380374f8f6b1722495980fd32b11
koskim1 <koskim1@naver.com>
2023-11-22 17:15:04 +0900

    문자열 크기 선언

--- test.cpp
+++ test.cpp
@@ -1,21 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-double a = 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1;
-double b = 1;
+// 문제 : 입력되는 문자열의 길이는 최대 100
+char str[100]; // Bad
+char str[101]; // GOOD
 
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    double value = a - b;
-    cout << value << "\n";
-    if (value < 1e-9) {
-        cout << "clear!\n";
-    }
+
 
 /*
--1.11022e-16
-clear!
+예를 들어 100개짜리 문자를 입력받는다. 또한 이걸 string이 아니라 char[] 로 한다면 char[101]로 선언해야 합니다.
+
+C++, C에서 문자는 null로 종료되는 것이 원칙이므로 마지막에 무조건 널문자인 ‘\0’에 해당하는 바이트가 붙습니다.
 */
 }

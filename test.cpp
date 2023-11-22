e305bd73685a2d7ff50668ea1f290b138325aa7d
koskim1 <koskim1@naver.com>
2023-11-22 17:14:38 +0900

    실수형 연산의 제한된 정확도

--- test.cpp
+++ test.cpp
@@ -1,20 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+double a = 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1;
+double b = 1;
+
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
+    double value = a - b;
+    cout << value << "\n";
+    if (value < 1e-9) {
+        cout << "clear!\n";
+    }
+
 /*
-항상 변수가 초기화가 되어있나 확인해야 합니다.
-
-ex) 문제에서 테스트케이스가 여러개 주어지고 주어진 테스트케이스 당... 하는 문제가 있습니다.
-
-그럴 경우 보통 다음과 같은 코드 그리고 while 루프 안에서 로직이 이어질텐데이 안에서 변수 초기화가 잘 되었는지 항상 확인합시다.
-
-cin >> t;
-while(t-);
-//로직
-
+-1.11022e-16
+clear!
 */
 }

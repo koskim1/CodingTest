8a797c440ceca50e397811d20788678077878137
koskim1 <koskim1@naver.com>
2023-11-24 23:57:43 +0900

    입력값이 따닥따닥 붙어있을때 입력받는법 - char타입 & cin ( 제일 추천하는 방법 )

--- test.cpp
+++ test.cpp
@@ -1,32 +1,27 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-char a[10][10];
+char a[54][54];
 
 int main(void) {
-    //ios::sync_with_stdio(0);
-    //cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 4; j++) {
-            scanf_s(" %c", &a[i][j]);
+            cin >> a[i][j];
         }
     }
-
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 4; j++) {
-            printf("%c", a[i][j]);
+            cout << a[i][j] << " ";
         }
-        printf("\n");
+        cout << "\n";
     }
-    return 0;
 }
 /*
-입력
 LLMM
 MMLL
-
-출력
-LLMM
-MMLL
+L L M M
+M M L L
 */

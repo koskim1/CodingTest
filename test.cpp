603d227ad60218faa571c2dd4978102ca7b975c9
koskim1 <koskim1@naver.com>
2023-11-24 23:57:12 +0900

    입력값이 따닥따닥 붙어있을때 입력받는법 - scanf로 받기

--- test.cpp
+++ test.cpp
@@ -1,36 +1,32 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int n, m, a[10][10];
-string s;
+char a[10][10];
 
 int main(void) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+    //ios::sync_with_stdio(0);
+    //cin.tie(0);
 
-    cin >> n >> m;
-    for (int i = 0; i < n; i++) {
-        cin >> s;
-        for (int j = 0; j < m; j++) {
-            a[i][j] = s[j] - '0';
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 4; j++) {
+            scanf_s(" %c", &a[i][j]);
         }
     }
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cout << a[i][j];
-        }
-        cout << '\n';
-    }
 
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("%c", a[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
 }
 /*
-4 4
-1000
-0000
-0111
-0000
-1000
-0000
-0111
-0000
+입력
+LLMM
+MMLL
+
+출력
+LLMM
+MMLL
 */

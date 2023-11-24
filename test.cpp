e3d785c9c55e641dd0c2551c1a2b9668e3e215ba
koskim1 <koskim1@naver.com>
2023-11-25 01:11:17 +0900

    Q. process exit call이란? ( 백준 일곱난쟁이 문제 예시 )

--- test.cpp
+++ test.cpp
@@ -1,27 +1,43 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-char a[54][54];
+
+/*
+프로그램이 실행이 되면 메모리에 올라가 프로세스가 됩니다. 이 프로세스를 종료시킨다라는
+의미입니다.
+*/
+
+int a[9];
+
+void printV(vector<int> v) {
+    for (int i : v) cout << i << "\n";
+}
+
+void combi(int start, vector<int>& b) {
+    if (b.size() == 7) {
+        int sum = accumulate(b.begin(), b.end(), 0);
+        if (sum == 100) {
+            printV(b);
+            exit(0);
+        }
+    }
+    for (int i = start + 1; i < 9; i++) {
+        b.push_back(a[i]);
+        combi(i, b);
+        b.pop_back();
+    }
+    return;
+}
 
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 4; j++) {
-            cin >> a[i][j];
-        }
-    }
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 4; j++) {
-            cout << a[i][j] << " ";
-        }
-        cout << "\n";
-    }
+    for (int i = 0; i < 9; i++) cin >> a[i];
+    sort(a, a + 9);
+    vector<int> v;
+    combi(-1, v);
+    return 0;
 }
 /*
-LLMM
-MMLL
-L L M M
-M M L L
 */

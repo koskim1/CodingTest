bbd35ffe9fdfc9756f3ddf7410ec5173d6432137
koskim1 <koskim1@naver.com>
2023-11-25 01:26:17 +0900

    입력값이 2차원 배열로 주어졌을때 방법

--- test.cpp
+++ test.cpp
@@ -1,43 +1,24 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-
-/*
-프로그램이 실행이 되면 메모리에 올라가 프로세스가 됩니다. 이 프로세스를 종료시킨다라는
-의미입니다.
-*/
-
-int a[9];
-
-void printV(vector<int> v) {
-    for (int i : v) cout << i << "\n";
-}
-
-void combi(int start, vector<int>& b) {
-    if (b.size() == 7) {
-        int sum = accumulate(b.begin(), b.end(), 0);
-        if (sum == 100) {
-            printV(b);
-            exit(0);
-        }
-    }
-    for (int i = start + 1; i < 9; i++) {
-        b.push_back(a[i]);
-        combi(i, b);
-        b.pop_back();
-    }
-    return;
-}
+vector<vector<int>> v = { {0,0,1}, {1,0,1}, {0,1,0} };
 
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    for (int i = 0; i < 9; i++) cin >> a[i];
-    sort(a, a + 9);
-    vector<int> v;
-    combi(-1, v);
+    int n = v.size();
+    int m = v[0].size();
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << v[i][j] << " ";
+        }
+        cout << "\n";
+    }
     return 0;
 }
 /*
+0 0 1
+1 0 1
+0 1 0
 */

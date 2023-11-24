457aef5c06c565b9e0a8c1277205187c9178e9f7
koskim1 <koskim1@naver.com>
2023-11-24 23:56:31 +0900

    입력값이 따닥따닥 붙어있을때 입력받는법 - string으로 변환

--- test.cpp
+++ test.cpp
@@ -1,51 +1,36 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
+int n, m, a[10][10];
+string s;
+
 int main(void) {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-/*
-### iostream의 자주 쓰는 함수
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> s;
+        for (int j = 0; j < m; j++) {
+            a[i][j] = s[j] - '0';
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << a[i][j];
+        }
+        cout << '\n';
+    }
 
-- swap
-- getline
-- clear
-- fill
-- tie
-- precision
-- sync_with_stdio
-- cin
-- cout
-- stdio.h의 함수들
-- printf
-- scanf
-- puts
-
-### string.h의 자주 쓰는 함수
-
-- memcpy
-- memset
-- size_t
-
-### algorithm의 자주 쓰는 함수
-
-- find
-- swap
-- fill
-- remove
-- unique
-- rotate
-- shuffle
-- sort
-- stable_sort
-- lower_bound
-- upper_bound
-- min
-- max
-- min_element
-- max_element
-- next_permutation
-- prev_permutation
-*/
 }
+/*
+4 4
+1000
+0000
+0111
+0000
+1000
+0000
+0111
+0000
+*/

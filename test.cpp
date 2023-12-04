0fdd443652753a304d4ca57e51cbc3f36d002cf4
koskim1 <koskim1@naver.com>
2023-12-04 22:11:23 +0900

    1-C  ( 2979 - 트럭 주차 )

--- test.cpp
+++ test.cpp
@@ -1,40 +1,37 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
+int A, B, C;
+int a, b;
+int cnt[104], ret;
 
-/*
-	재귀함수의 시간복잡도는 재귀함수의 메인로직에 있다.
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
 
-	MainLogic * 함수호출,,
-	메인로직에 대한 함수호출이 몇번 호출되냐에 따라 시간복잡도가 정해짐
-*/
-
-int N, cnt;
-
-void solve(int N) {
-	cnt++;
-	cout << cnt << '\n';
-	if (N == 0) return;
+	cin >> A >> B >> C;
 	for (int i = 0; i < 3; i++) {
-		solve(N - 1);
+		cin >> a >> b;
+		for (int j = a; j < b; j++) cnt[j]++;
 	}
-	return;
+
+	for (int j = 1; j < 100; j++) {
+		if (cnt[j]) {
+			if (cnt[j] == 1) ret += A;
+			else if (cnt[j] == 2) ret += B * 2;
+			else if (cnt[j] == 3) ret += C * 3;
+		}
+	}
+	cout << ret << "\n";
+
 }
+/*
+입력
+5 3 1
+1 6
+3 5
+2 8
 
-
-int main(void) {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-	cin >> N;
-	solve(N);
-
-	// 등비수열 문제이니
-	// a(r의n승 -1) / (r - 1)
-
-	// 이 문제의 메인로직은 O(1)을 가지고 있고
-	// 함수는 등비수열 만큼 즉 2분의 1의 ( 3의 n승 - 1인데)
-	// 상수 다 띄고나면 3의 n승이고
-	// 메인로직 X 함수는 => O(1) * O(3^n)이니 
-	// 이 식의 시간복잡도는 O(3^n)이다.
-}
+출력
+33
+*/

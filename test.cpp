0d91a40a680a4b018c9cd6c1cfd354ab853df9b6
koskim1 <koskim1@naver.com>
2023-12-07 23:38:39 +0900

    1-D 10988 팰린드롬인지 확인하기

--- test.cpp
+++ test.cpp
@@ -1,37 +1,18 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int A, B, C;
-int a, b;
-int cnt[104], ret;
+string a;
+string b;
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	cin >> A >> B >> C;
-	for (int i = 0; i < 3; i++) {
-		cin >> a >> b;
-		for (int j = a; j < b; j++) cnt[j]++;
-	}
+	cin >> a;
+	b = a;
+	reverse(b.begin(), b.end());
 
-	for (int j = 1; j < 100; j++) {
-		if (cnt[j]) {
-			if (cnt[j] == 1) ret += A;
-			else if (cnt[j] == 2) ret += B * 2;
-			else if (cnt[j] == 3) ret += C * 3;
-		}
-	}
-	cout << ret << "\n";
+	if (a == b)cout << 1 << "\n";
+	else cout << 0 << "\n";
 
 }
-/*
-입력
-5 3 1
-1 6
-3 5
-2 8
-
-출력
-33
-*/

f291952cd73617a9bde1018970e436b6054741ce
koskim1 <koskim1@naver.com>
2023-12-07 23:40:24 +0900

    1-G 9996 한국이 그리울 땐 서버에 접속하지 ( 내 틀린 풀이법 )

--- test.cpp
+++ test.cpp
@@ -1,47 +1,36 @@
-﻿#include <bits/stdc++.h>
+﻿// 나만의 틀린 풀이법임
+/*첫번째 실수
+a* b 이렇게 예시 주어졌다 해서 당연히 한글자만 로직이 되는거 아님
+ab* ab처럼 두글자도 나올수있음 이걸 생각못한 나는 처음부터 틀림 ㅋ;
+*/
+#include <bits/stdc++.h>
 using namespace std;
 
-string s;
+// N은 1이상 100이하 정수
+int N;
+// 카운팅 해야하니. begin, end에 cnt[] ++ 해주면 될듯?
+// 찾아보니 string에는 .front() 맨앞 , .back() 맨뒤 이게 있네
+string s, ex, res;
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	getline(cin, s);
-	for (int i = 0; i < s.size(); i++) {
-		// 대문자인 경우
-		if (s[i] >= 65 && s[i] < 97) {
-			if (s[i] + 13 > 90) s[i] = s[i] + 13 - 26;
-			else s[i] = s[i] + 13;
-		}
-		else if (s[i] >= 97 && s[i] <= 122) {
-			if (s[i] + 13 > 122)s[i] = s[i] + 13 - 26;
-			else s[i] = s[i] + 13;
-		}
-	}
-	for (int i = 0; i < s.size(); i++) {
-		cout << s[i];
-	}
-
-	/*
-	* // 예외사항을 생각안하고있었음
-	* 카운팅을 하는것이 아닌 그냥 바로 고쳤으면 됐음
-	* 아스키 코드 잘 생각할것 A = 65 a = 97 알파벳은 26개임
-	*
-	*
-	cin >> s;
-	for (int i = 0; i < s.size(); i++) {
-		if (s[i] >= 97) {
-			cnt[i - 'a' + 13];
-		}
-		else if (s[i] < 97) {
-			cnt[i + 'A' + 13];
+	cin >> N;
+	cin >> ex;
+	char a = ex.front();
+	char b = ex.back();
+	if (N >= 1 && N <= 100) {
+		for (int i = 0; i < N; i++) {
+			cin >> s;
+			for (int i = 0; i < 1; i++) {
+				if (s.front() == a && s.back() == b) {
+					cout << "DA";
+				}
+				else {
+					cout << "NE";
+				}
+			}
 		}
 	}
-	for (int i = 0; i < s.size(); i++) {
-		cout << cnt[i];
-	}
-	cout << "\n";
-
-	*/
 }

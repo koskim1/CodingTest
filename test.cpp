8c7258d2ce9637585920f56ae376ace2d0e0141d
koskim1 <koskim1@naver.com>
2023-12-07 23:39:48 +0900

    1-F 11655 ROT13

--- test.cpp
+++ test.cpp
@@ -1,26 +1,47 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-int n, cnt[26];
-string s, ret;
+string s;
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	cin >> n;
-	//첫번째 인덱스의 알파벳, 즉 성이 같은 알파벳 5개이상 찾아내야함
-	// 카운팅배열이 생각나야함
-	for (int i = 0; i < n; i++) {
-		cin >> s;
-
-		// 어떻게 첫번째 인덱스만 빼올것인지?
-		cnt[s[0] - 'a']++;
+	getline(cin, s);
+	for (int i = 0; i < s.size(); i++) {
+		// 대문자인 경우
+		if (s[i] >= 65 && s[i] < 97) {
+			if (s[i] + 13 > 90) s[i] = s[i] + 13 - 26;
+			else s[i] = s[i] + 13;
+		}
+		else if (s[i] >= 97 && s[i] <= 122) {
+			if (s[i] + 13 > 122)s[i] = s[i] + 13 - 26;
+			else s[i] = s[i] + 13;
+		}
 	}
-	// 5개 이상인것들을 어디서 보관해서 확인할것인지?
-	for (int i = 0; i < 26; i++) if (cnt[i] >= 5) ret += (i + 'a');
-	// 5개이상 찾아낸것들 사전적으로 순차적출력
-	if (ret.size()) cout << ret << "\n";
-	// 없으면 PREDAJA 출력
-	else cout << "PREDAJA" << "\n";
+	for (int i = 0; i < s.size(); i++) {
+		cout << s[i];
+	}
+
+	/*
+	* // 예외사항을 생각안하고있었음
+	* 카운팅을 하는것이 아닌 그냥 바로 고쳤으면 됐음
+	* 아스키 코드 잘 생각할것 A = 65 a = 97 알파벳은 26개임
+	*
+	*
+	cin >> s;
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] >= 97) {
+			cnt[i - 'a' + 13];
+		}
+		else if (s[i] < 97) {
+			cnt[i + 'A' + 13];
+		}
+	}
+	for (int i = 0; i < s.size(); i++) {
+		cout << cnt[i];
+	}
+	cout << "\n";
+
+	*/
 }

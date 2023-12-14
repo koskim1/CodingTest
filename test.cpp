dec6c583aa2e9cea9d429a63f736b8e9c624189c
koskim1 <koskim1@naver.com>
2023-12-14 16:59:20 +0900

    1-O 4375 1 @풀이 살짝 첨가@

--- test.cpp
+++ test.cpp
@@ -10,6 +10,7 @@ int main() {
 	//ios::sync_with_stdio(0);
 	//cin.tie(0);
 
+	// while(cin >> n)으로 해도 됨 / 입력값 계속 받을땐 EOF사용
 	while (scanf("%d", &n) != EOF) {
 		ll cnt = 1, ret = 1;
 		while (true) {
@@ -18,7 +19,9 @@ int main() {
 				break;
 			}
 			else {
+				// 1이 아니라면 11로,, 11아니며 111로,,, ( 모듈러 연산 해주면서 )
 				cnt = (cnt * 10) % n + 1 % n;
+				// 1이 몇개인지 카운트 ( 가장 작은 자리수 )
 				ret++;
 			}
 		}

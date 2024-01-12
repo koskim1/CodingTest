371e705e58e866a8bdc3b627bbcb5b55d045a3d6
koskim1 <koskim1@naver.com>
2024-01-12 23:55:09 +0900

    Q. 인접리스트를 기반으로 탐색하기

--- test.cpp
+++ test.cpp
@@ -1,46 +1,46 @@
 ﻿#include <bits/stdc++.h>
 using namespace std;
 
-const int V = 4;
-// 벡터가 여러개 있는것 정점마다 연결리스트가 필요하니깐,,
-vector<int> adj[V];
+/*
+1번.
+정점은 0번부터 9번까지 10개의 노드가 있다. 1 - 2 /  1- 3 / 3 - 4라는 경로가 있다. 
+(1번과 2번, 1번과 3번, 3번과 4번은 연결되어 있다 )
+이를 인접리스트로 표현한다면?
 
+2번.
+0번부터 방문안한 노드를 찾고 해당 노드부터 방문, 연결된 노드를 이어서 방문해서 
+출력하는 재귀함수를 만들고 싶다면 어떻게 해야할까?
+또한, 정점을 방문하고나서 다시 방문하지 않게 만드려면 어떻게 해야할까?
+*/
+
+const int V = 10;
+vector<int> adj[V];
+int visited[V];
+
+void go(int idx) {
+	cout << idx << '\n';
+	visited[idx] = 1;
+	for (int there : adj[idx]) {
+		if (visited[there]) continue;
+		go(there);
+	}
+}
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	adj[0].push_back(1);
-	adj[0].push_back(2);
-	adj[0].push_back(3);
-
-	adj[1].push_back(0);
 	adj[1].push_back(2);
-
-	adj[2].push_back(0);
 	adj[2].push_back(1);
 
-	adj[3].push_back(0);
+	adj[1].push_back(3);
+	adj[3].push_back(1);
 
-	for (int i = 0; i < 4; i++) {
-		cout << i << " :: ";
-		for (int there : adj[i]) {
-			cout << there << " ";
-		}
-		cout << '\n';
-	}
-	// 이렇게도 할 수 있다.
-	for (int i = 0; i < 4; i++) {
-		cout << i << " :: ";
-		for (int j = 0; j < adj[i].size(); j++) {
-			cout << adj[i][j] << " ";
-		}
-		cout << '\n';
+	adj[3].push_back(4);
+	adj[4].push_back(3);
+
+	for (int i = 0; i < V; i++) {
+		if (adj[i].size() && visited[i] == 0)
+			go(i);
 	}
 }
-/*
-0 :: 1 2 3
-1 :: 0 2
-2 :: 0 1
-3 :: 0
-*/

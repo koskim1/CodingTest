79463c9e6a1d754f5e84d89b969e435573b8260a
koskim1 <koskim1@naver.com>
2024-11-20 22:45:24 +0900

    그냥 STL쌩으로 함수 인자에 넣으면 복사해서 보냄 &해서 참조

--- test.cpp
+++ test.cpp
@@ -12,63 +12,18 @@ void fastIO() {
     cout.tie(NULL);
 }
 
-/*
-단백질 mp 100
-지방 mf 70
-탄수화물 ms 90
-비타민 mv 10
-을 넘겨야하는데 그 중 최소가 되는 번호들로.
+vector<int> v(100);
 
-
-*/
-
-int n, mp, mf, ms, mv;
-int b, c, d, e, ret = INF, sum;
-
-struct A {
-    int mp, mf, ms, mv, cost;
-}a[16];
-
-map<int, vector<vector<int>>> ret_v;
+void func(vector<int> &v) {
+    v[10] = 7;
+}
 
 int main() {
     fastIO();
 
-    cin >> n;
-    cin >> mp >> mf >> ms >> mv;
-    for (int i = 0; i < n; i++) {
-        cin >> a[i].mp >> a[i].mf >> a[i].ms >> a[i].mv >> a[i].cost;
-    }
-
-    for (int i = 1; i < (1 << n); i++) {
-        b = c = d = e = sum = 0;
-        vector<int> v;
-        for (int j = 0; j < n; j++) {
-            if (i & (1 << j)) {
-                v.push_back(j + 1);
-                b += a[j].mp;
-                c += a[j].mf;
-                d += a[j].ms;
-                e += a[j].mv;
-                sum += a[j].cost;
-            }
-        }
-        if (b >= mp && c >= mf && d >= ms && e >= mv) {
-            if (ret >= sum) {
-                ret = sum;
-                ret_v[ret].push_back(v);
-            }
-        }
-    }
-
-    if (ret == INF) cout << -1 << '\n';
-    else {
-        sort(ret_v[ret].begin(), ret_v[ret].end());
-        cout << ret << "\n";
-        for (int a : ret_v[ret][0]) {
-            cout << a << " ";
-        }
-    }
-
+    
+    func(v);
+    cout << v[10] << "\n";
+    
     
 }

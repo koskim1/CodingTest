2ea40b56f2ad2e835edae12294e48d02ac91c8fc
koskim1 <koskim1@naver.com>
2024-12-03 23:24:08 +0900

    큐관련 2164

--- test.cpp
+++ test.cpp
@@ -12,32 +12,24 @@ void fastIO() {
     cout.tie(NULL);
 }
 
-int n, num;
-
+int n;
+queue<int> q;
 int main() {
     fastIO();
-    
-    stack<pair<int, int>> stk; // { 탑의 높이, 탑의 인덱스 }
 
     cin >> n;
-    vector<int> v(n, 0);//사이즈 n만큼 0으로 채워둬. 이따가 안부딪히는거(앞쪽)는 0으로 하면 되고 나머진 덮으면 되니껜
 
-    for (int i = 0; i < n; i++) {
-        cin >> num;
-
-        while (!stk.empty() && stk.top().first <= num) {
-            stk.pop();
-        }
-
-        if (!stk.empty()) {
-            v[i] = stk.top().second + 1; // 몇번째에 있었는지
-        }
-
-        stk.push({ num, i });
+    for (int i = 1; i <= n; i++) {
+        q.push(i);
     }
 
-    for (int i = 0; i < n; i++) {
-        cout << v[i] << " ";
+    while (n != 1) {
+        q.pop();
+        if (q.size() == 1) break;
+        // 맨 위에있는 카드 맨 밑으로
+        int temp = q.front();
+        q.pop();
+        q.push(temp);
     }
-
+    cout << q.front();
 }

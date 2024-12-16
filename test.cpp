f5a19f7fb5a878c5188c173ddf6b356c0c7b55d9
koskim1 <koskim1@naver.com>
2024-12-16 17:03:29 +0900

    7562 나이트의 이동(bfs구현 좀 잘한듯?)

--- test.cpp
+++ test.cpp
@@ -12,52 +12,70 @@ void fastIO() {
 	cout.tie(NULL);
 }
 
-const int dy[4] = { -1,0,1,0 };
-//const int dx[4] = { 0,1,0,-1 };
+/*
+입력값 :
+1. 총 TC 개수
+2. 체스판의 한 변의 길이(정사각형)
+3. 현재 나이트가 있는 칸
+4. 나이트가 이동하려는 칸
+5. 각 TC마다 최소 몇번만에 이동할 수 있는가 출력
+*/
 
-#define MAX 1000004
+const int MAX = 304;
+const int dy[8] = { -1,-2,-2,-1,1,2,1,2 };
+const int dx[8] = { -2,-1,1,2,2,1,-2,-1 };
 
-int f, s, g, u, d;
-int dx[2];
-int visited[MAX];
-bool flag = true;
+int TC, n;
+bool visited[MAX][MAX];
 
-void bfs() {
-	queue<int> q;
-	q.push(s);
-	visited[s] = 1;
+int bfs(int startY, int startX, int targetY, int targetX) {
+	queue<pair<int, int>> q;
+	queue<int> dist; //이동횟수 저장
+	visited[startY][startX] = true;
+	q.push({ startY,startX });
+	dist.push(0);
 
 	while (!q.empty()) {
-		s = q.front(); q.pop();
+		// 변수 저장
+		int y = q.front().first;
+		int x = q.front().second;
+		int steps = dist.front();
+		q.pop();
+		dist.pop();
 
-		for (int i = 0; i < 2; i++) {
-			int nx = s + dx[i];
+		// 기저사례
+		if (y == targetY && x == targetX) {
+			return steps;
+		}
 
-			if (nx > 0 && nx <= f) {
-				if (visited[nx] == 0) {
-					visited[nx] = visited[s] + 1;
-					q.push(nx);
-				}
-			}
+		// bfs
+		for (int i = 0; i < 8; i++) {
+			int ny = y + dy[i];
+			int nx = x + dx[i];
 
-			if (s == g) {
-				flag = false;
-				break;
+			if (nx >= 0 && nx < n && ny >= 0 && ny < n && !visited[ny][nx]) {
+				q.push({ ny,nx });
+				dist.push(steps + 1);
+				visited[ny][nx] = true;
 			}
 		}
 	}
+	// 안되면
+	return -1;
 }
 
 int main() {
 	fastIO();
 
-	cin >> f >> s >> g >> u >> d;
-	dx[0] = u;
-	dx[1] = -d;
+	cin >> TC;
+	while (TC--) {
+		cin >> n;
+		int startY, startX, targetY, targetX;
+		cin >> startY >> startX >> targetY >> targetX;
 
-	bfs();
+		// TC돌리니 매 TC마다 초기화 시켜주고 bfs돌리기
+		memset(visited, false, sizeof(visited));
 
-	if (flag) cout << "use the stairs";
-	else cout << visited[g] - 1;
-
+		cout << bfs(startY, startX, targetY, targetX) << "\n";
+	}
 }

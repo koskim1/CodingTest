828bfc973cd17dae0b5a630ba0c6d89f6a2fe7ce
koskim1 <koskim1@naver.com>
2024-12-17 17:33:06 +0900

    10026 적록색약 (bfs문안에 if문 참고, memset으로 배열 초기화)

--- test.cpp
+++ test.cpp
@@ -12,70 +12,85 @@ void fastIO() {
 	cout.tie(NULL);
 }
 
-/*
-입력값 :
-1. 총 TC 개수
-2. 체스판의 한 변의 길이(정사각형)
-3. 현재 나이트가 있는 칸
-4. 나이트가 이동하려는 칸
-5. 각 TC마다 최소 몇번만에 이동할 수 있는가 출력
-*/
+const int dy[4] = { -1,0,1,0 };
+const int dx[4] = { 0,1,0,-1 };
 
-const int MAX = 304;
-const int dy[8] = { -1,-2,-2,-1,1,2,1,2 };
-const int dx[8] = { -2,-1,1,2,2,1,-2,-1 };
+int n;
+char board[104][104];
+bool visited[104][104];
 
-int TC, n;
-bool visited[MAX][MAX];
-
-int bfs(int startY, int startX, int targetY, int targetX) {
+void bfs(int y, int x, bool isColorWeakness) {
 	queue<pair<int, int>> q;
-	queue<int> dist; //이동횟수 저장
-	visited[startY][startX] = true;
-	q.push({ startY,startX });
-	dist.push(0);
+	q.push({ y,x });
+	visited[y][x] = true;
+	char color = board[y][x];
 
 	while (!q.empty()) {
-		// 변수 저장
-		int y = q.front().first;
-		int x = q.front().second;
-		int steps = dist.front();
+		int cy = q.front().first;
+		int cx = q.front().second;
 		q.pop();
-		dist.pop();
 
-		// 기저사례
-		if (y == targetY && x == targetX) {
-			return steps;
-		}
+		for (int i = 0; i < 4; i++) {
+			int ny = cy + dy[i];
+			int nx = cx + dx[i];
 
-		// bfs
-		for (int i = 0; i < 8; i++) {
-			int ny = y + dy[i];
-			int nx = x + dx[i];
+			if (ny < 0 || ny >= n || nx < 0 || nx >= n)continue;
+			if (visited[ny][nx])continue;
 
-			if (nx >= 0 && nx < n && ny >= 0 && ny < n && !visited[ny][nx]) {
-				q.push({ ny,nx });
-				dist.push(steps + 1);
-				visited[ny][nx] = true;
+			//일반인
+			if (!isColorWeakness) {
+				if (board[ny][nx] == color) {
+					visited[ny][nx] = true;
+					q.push({ ny,nx });
+				}
+			}
+			//적록색약
+			else {
+				if ((color == 'R' || color == 'G') && (board[ny][nx] == 'R' || board[ny][nx] == 'G')) {
+					visited[ny][nx] = true;
+					q.push({ ny,nx });
+				}
+				else if (color == 'B' && board[ny][nx] == 'B') {
+					visited[ny][nx] = true;
+					q.push({ ny,nx });
+				}
 			}
 		}
 	}
-	// 안되면
-	return -1;
 }
+
 
 int main() {
 	fastIO();
 
-	cin >> TC;
-	while (TC--) {
-		cin >> n;
-		int startY, startX, targetY, targetX;
-		cin >> startY >> startX >> targetY >> targetX;
-
-		// TC돌리니 매 TC마다 초기화 시켜주고 bfs돌리기
-		memset(visited, false, sizeof(visited));
-
-		cout << bfs(startY, startX, targetY, targetX) << "\n";
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> board[i][j];
+		}
 	}
+	int normalCount = 0;
+	int colorWeaknessCount = 0;
+
+	memset(visited, false, sizeof(visited));
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (!visited[i][j]) {
+				bfs(i, j, true);
+				colorWeaknessCount++;
+			}
+		}
+	}
+
+	memset(visited, false, sizeof(visited));
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (!visited[i][j]) {
+				bfs(i, j, false);
+				normalCount++;
+			}
+		}
+	}
+
+	cout << normalCount << " " << colorWeaknessCount << "\n";
 }

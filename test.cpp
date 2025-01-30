818a1384bbff167ecec91372f74a018df47102e7
koskim1 <koskim1@naver.com>
2025-01-30 23:11:17 +0900

    DP 1149 RGB거리 (+바킹독 버전 및 설명 )

--- test.cpp
+++ test.cpp
@@ -15,37 +15,42 @@ void fastIO() {
 
 /*
 1.테이블 정의하기
+D[i][0] = i번째 집을 R로 칠할 때 최소비용
+D[i][1] = i번째 집을 G로 칠할 때 최소비용
+D[i][2] = i번째 집을 B로 칠할 때 최소비용
 
 2.점화식 세우기
+D[k][0] = min(D[k-1][1],D[k-1][2]) + R[k];
+D[k][1] = min(D[k-1][0],D[k-1][2]) + G[k];
+D[k][2] = min(D[k-1][0],D[k-1][1]) + B[k];
 
 3.초기값 설정하기
+D[1][0] = R[1];
+D[1][1] = G[1];
+D[1][2] = B[1];
 
 4.점화식을 이용해 문제 풀기
 */
 
-int n;
+int d[1005][3];
+int r[1005], g[1005], b[1005];
 
 int main() {
 	fastIO();
 
+	int n;
 	cin >> n;
-	vector<vector<int>> costs(n, vector<int>(3));
 
-	for (int i = 0; i < n; ++i) {
-		cin >> costs[i][0] >> costs[i][1] >> costs[i][2];
+	for (int i = 1; i <= n; i++) cin >> r[i] >> g[i] >> b[i];
+	d[1][0] = r[1];
+	d[1][1] = g[1];
+	d[1][2] = b[1];
+
+	for (int i = 2; i <= n; i++) {
+		d[i][0] = min(d[i - 1][1], d[i - 1][2]) + r[i];
+		d[i][1] = min(d[i - 1][0], d[i - 1][2]) + g[i];
+		d[i][2] = min(d[i - 1][0], d[i - 1][1]) + b[i];
 	}
 
-	vector<vector<int>> dp(n, vector<int>(3));
-	dp[0][0] = costs[0][0];
-	dp[0][1] = costs[0][1];
-	dp[0][2] = costs[0][2];
-
-	for (int i = 1; i < n; ++i) {
-		dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + costs[i][0];	// R로 칠하는경우
-		dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + costs[i][1];	// G로 칠하는경우
-		dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + costs[i][2];	// B로 칠하는경우
-	}
-
-	int result = min({ dp[n - 1][0], dp[n - 1][1], dp[n - 1][2] });
-	cout << result << '\n';
+	cout << min({ d[n][0],d[n][1],d[n][2] });
 }
